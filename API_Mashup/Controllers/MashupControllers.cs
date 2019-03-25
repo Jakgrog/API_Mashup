6e114abf8c329bca187a2f01af6f51dbcf07dc24
Jakob Grönlund <jakgr008@student.liu.se>
2019-03-25 23:50:31 +0100

    API mashup - Only mockdata

---
+++ API_Mashup/Controllers/MashupControllers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace API_Mashup
+{
+    public class MashupController : ApiController
+    {
+        [HttpGet]
+        public ArtistInformation Get(string id)
+        {
+            List<Album> Albums = new List<Album>();
+            Albums.Add(new Album("Songalong","ididid", "http://image.jpg"));
+
+            return new ArtistInformation(id, "Jakob", Albums);
+        }
+    }
+}
---
+++ API_Mashup/Global.asax.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace API_Mashup
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        //Called everytime the web-app is started
+        protected void Application_Start()
+        {
+            //Always at top (Before any MVC route config)
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
